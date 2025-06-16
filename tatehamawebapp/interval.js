0d75b1333cb72c651a6bd876526cf33d3c23508a
uiro_sr <uiro9503@gmail.com>
2025-06-17 01:12:34 +0900

    10秒に一回きえましゅ

--- tatehamawebapp/interval.js
+++ tatehamawebapp/interval.js
@@ -1,6 +1,12 @@
 
 async function Interval() {
+
     await Getdata();
+    // Train_icon_containerクラスの全要素の中身を消去
+    const containers = document.getElementsByClassName('train-icon-container');
+    for (let i = 0; i < containers.length; i++) {
+        containers[i].innerHTML = '';
+    }
     placeAllTrainIconsByLocation();
     updateTime();
 }
