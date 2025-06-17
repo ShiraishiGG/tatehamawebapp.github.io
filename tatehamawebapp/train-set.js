067559a88a060887f38ae62e39c218f2dd4596d9
uiro_sr <uiro9503@gmail.com>
2025-06-18 01:38:06 +0900

    gozitteta

--- tatehamawebapp/train-set.js
+++ tatehamawebapp/train-set.js
@@ -113,7 +113,7 @@ function TypeString(retsuban) {
 
     if (Retsuban.startsWith("臨")) {
         if (Retsuban.includes("A")) {
-            return 23ri;
+            return 23;
         }
         if (Retsuban.includes("K")) {
             return 9;
