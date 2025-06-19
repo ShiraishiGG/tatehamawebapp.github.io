4d5d4c7c4f276d72abbe26ee78056d84f523be4c
TakumiteTudo <takumit2886@gmail.com>
2025-06-20 03:13:24 +0900

    フィルタリング対応

--- tatehamawebapp/information.js
+++ tatehamawebapp/information.js
@@ -6,10 +6,17 @@ const DEFAULT_INFORMATION_TEXT = `【館浜本線 運転会時間外】
 
 // 運行情報テキスト
 window.informationtexts = [{ "Type": 1, Content: DEFAULT_INFORMATION_TEXT }];
-window.informationindex = 0;
+window.informationindex = -1;
 
 function updateInformationText() {
-    window.informationtexts = Location_data.OperationInformations.length != 0 ? Location_data.OperationInformations : [{"Type": 1 ,Content: DEFAULT_INFORMATION_TEXT }]
+    const infos = Location_data.OperationInformations;
+    if (Array.isArray(infos) && infos.length > 0) {
+        // Typeが2以上のものがあるか
+        const filtered = infos.filter(info => info.Type >= 2);
+        window.informationtexts = filtered.length > 0 ? filtered : infos;
+    } else {
+        window.informationtexts = [{ "Type": 1, Content: DEFAULT_INFORMATION_TEXT }];
+    }
 }
 
 function printInformationText() {
@@ -24,7 +31,7 @@ function printInformationText() {
 
     console.log("運行情報の表示番号: ", window.informationtexts);
     console.log("運行情報の表示番号: ", window.informationindex);
-    let textToShow = (window.informationtexts[window.informationindex].Content.trim()) ? window.informationtexts[window.informationindex].Content : DEFAULT_INFORMATION_TEXT;
+    window.textToShow = (window.informationtexts[window.informationindex].Content.trim()) ? window.informationtexts[window.informationindex].Content : DEFAULT_INFORMATION_TEXT;
 
     if (elBottom) {
         elBottom.textContent = textToShow.replace("\n", "");
@@ -54,7 +61,6 @@ document.addEventListener('DOMContentLoaded', function () {
         if (area && modal && modalBody && modalClose) {
             area.addEventListener('click', function () {
                 // モーダル表示時も最新内容に
-                let textToShow = (window.informationtexts[window.informationindex].Content.trim()) ? window.informationtexts[window.informationindex].Content : DEFAULT_INFORMATION_TEXT;
                 modalBody.innerHTML = `
                     <h2>運行情報</h2>
                     <div class="info-modal-message">${textToShow.replace(/\r\n/g, '<br/>')}</div>
